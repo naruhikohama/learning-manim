849c52505b155b3500848f94b1b3e43fdd460377
naruhikohama <naruhikohama@gmail.com>
2023-10-22 16:25:17 -0300

    simple animations in manim

--- benjamin-hackl-tutorial/first-example.py
+++ benjamin-hackl-tutorial/first-example.py
@@ -4,5 +4,32 @@ class FirstExample(Scene):
     def construct(self):
         ax = Axes(x_range=(-3, 3), y_range=(-3, 3))
         curve = ax.plot(lambda x: (x + +2)*x*(x-2)/2, color=RED)
-        area = ax.get_area(curve, x_range=(-2, 0), color=LIGHT_GRAY, opacity=0.2)
-        self.add(ax, curve, area)
+        area = ax.get_area(curve, x_range=(-2, 0))
+
+        self.play(Create(ax, run_time = 2)) # run_time inside can be inside Create() and is optional
+        self.wait(0.75)
+        self.play(Create(curve), run_time=3) # run_time outside Create(), and inside play(). Also optional
+        self.wait(0.75)
+        self.play(FadeIn(area))
+        self.wait(2)
+        # self.add(ax, curve, area)
+
+class SquareToCircle(Scene):
+    def construct(self):
+        green_square = Square(color=GREEN, fill_opacity=0.5)
+        self.play(DrawBorderThenFill(green_square))
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        self.play(Transform(green_square, blue_circle))
+        self.wait(2)
+
+
+class SquareToCircle2(Scene):
+    def construct(self):
+        square = Square()
+        circle = Circle()
+        square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+        circle.set_fill(PINK, opacity=0.5)
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
