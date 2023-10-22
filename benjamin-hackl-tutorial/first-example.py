372b446cb99aaa3745ab07966dcf449ce9fb7b62
naruhikohama <naruhikohama@gmail.com>
2023-10-22 16:08:15 -0300

    adding some elements to scene

--- benjamin-hackl-tutorial/first-example.py
+++ benjamin-hackl-tutorial/first-example.py
@@ -3,5 +3,6 @@ from manim import *
 class FirstExample(Scene):
     def construct(self):
         ax = Axes(x_range=(-3, 3), y_range=(-3, 3))
-
-        self.add(ax)
+        curve = ax.plot(lambda x: (x + +2)*x*(x-2)/2, color=RED)
+        area = ax.get_area(curve, x_range=(-2, 0), color=LIGHT_GRAY, opacity=0.2)
+        self.add(ax, curve, area)
